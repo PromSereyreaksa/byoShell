c05ea105ae72ccf6ff3aa5d51b61f4e0c2ee6197
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-02-17 20:14:36 +0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,30 +1,49 @@
 const { exit } = require("process");
 const readline = require("readline");
 
+// Builtin commands
+const builtins = {
+  exit: () => {
+    console.log("Exiting byoShell...");
+    process.exit(0);
+  },
+  echo: (args) => {
+    console.log(args.join(" "));
+  },
+};
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: "$ ",
 });
 
-
+// show $ initial prompt
 rl.prompt();
 
+rl.on("line", (input) => {
+  const trimmed = input.trim();
 
-rl.on("line", (command) => {
-  command.trim();
-
-  if (command.length === 0) {
+  // if user enter just go back to $ prompt
+  if (!trimmed) {
     rl.prompt();
     return;
   }
 
-  if (command == 'exit') {
-    exit();
+  // .split(/\s+/) splits the string into an array of words, using any amount of whitespace as the separator
+  const [cmd, ...args] = trimmed.split(/\s+/);
+
+  if (builtins[cmd]) {
+    builtins[cmd](args);
+  } else {
+    console.log(`${cmd}: command not found`);
   }
 
-  console.log(`${command}: command not found`);
-  
   rl.prompt();
 });
 
+rl.on("SIGINT", () => {
+  console.log("\nExiting Shell...");
+  rl.close();
+  process.exit(0);
+});
