a1c8c74aa88591469d34b544c6ad98df63cfdb8c
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-02-17 21:17:50 +0700

    codecrafters submit [skip ci]

--- app/utils/findExecutable.js
+++ app/utils/findExecutable.js
@@ -2,20 +2,18 @@ import fs from "fs";
 import path from "path";
 
 export function findExecutable(command) {
-    const PATH = process.env.path || "";
-    const dirs = PATH.split(":");
+  const PATH = process.env.PATH || "";
+  const dirs = PATH.split(":");
 
-    for (const dir of dirs) {
-        const fullPath = path.join(dir, command);
-        if (fs.existsSync(fullPath)) {
-            try {
-                fs.accessSync(fullPath, fs.constants.x_OK);
-                return fullPath;
-            } catch {
-                // if empty skip
-            }
-        }
+  for (const dir of dirs) {
+    const fullPath = path.join(dir, command);
+    if (fs.existsSync(fullPath)) {
+      try {
+        fs.accessSync(fullPath, fs.constants.X_OK);
+        return fullPath;
+      } catch {}
     }
+  }
 
-    return null;
+  return null;
 }
