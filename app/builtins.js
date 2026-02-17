fd8fb28abde5f88ce61d69788266f0d5ff92455a
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-02-17 22:00:13 +0700

    codecrafters submit [skip ci]

--- app/builtins.js
+++ app/builtins.js
@@ -1,6 +1,7 @@
 import { exit } from "process";
 import { findExecutable } from "./utils/findExecutable.js";
 import fs from "fs";
+import path from "path"
 
 export const builtins = {
   exit: () => {
@@ -13,25 +14,30 @@ export const builtins = {
     console.log(process.cwd());
   },
   cd: (args) => {
-      if (!args || args.length === 0) {
-          return;
-      }
+    if (!args || args.length === 0) {
+      return;
+    }
 
-        const target = args[0];
-        
-        // only handle absolute path for now
-      if (target.startsWith("/")) {
-          try {
-              if (fs.existsSync(target) && fs.statSync(target).isDirectory) {
-                  process.chdir(target);
-              } else {
-                  console.log(`cd: ${target}: No such file or directory`);
-              }
-          } catch {
-              console.log(`cd: ${target}: No such file or directory`);
-            }
-        }
-    
+    const target = args[0];
+
+    // resolve relative paths against current working dir
+    // The process.cwd() method returns the current working directory of the Node.js process.
+    const resolvedPath = path.resolve(process.cwd(), target); // handles absolute path, relative paths, ./ , ../, nested relative path atuomatically
+
+    try {
+      // so we are checking if the resolvedPath exist existsSync will return true
+      // and statSync will check if it is a directory
+      if (
+        fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isDirectory
+      ) {
+        // The process.chdir() method changes the current working directory of the Node.js process or throws an exception if doing so fails (for instance, if the specified directory does not exist).
+        process.chdir(resolvedPath); //if true we change directory to the path
+      } else {
+        console.log(`cd: ${target}: No such file or directory`);
+      }
+    } catch {
+      console.log(`cd: ${target}: No such file or directory`);
+    }
   },
   type: (args) => {
     if (!args || args.length === 0) {
